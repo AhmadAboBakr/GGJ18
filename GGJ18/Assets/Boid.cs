3307523fc479438197c6f49e72eb2707a46f6897
ahmadabobakr <ahmadabobakr@gmail.com>
2018-01-26 23:40:02 +0200

    flock working

---
+++ GGJ18/Assets/Boid.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    public FlockManager target;
+    public float minDistacne;
+    public float maxSpeed = 10;
+    public float maxDistanceFromSwarm = 4;
+    private Vector3 dir;
+    Rigidbody rigidBody;
+    // Use this for initialization
+    void Start () {
+        rigidBody = this.GetComponent<Rigidbody>();
+        target.followers.Add(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var seek = target.queen.transform.position - this.transform.position;
+        var seperation = Seperate();
+
+        dir = seek.normalized +seperation*.5f;
+        var toTarget= (target.queen.transform.position - this.transform.position);
+        if (toTarget.sqrMagnitude > maxDistanceFromSwarm)
+        {
+            //dir /= toTarget.sqrMagnitude;
+        }
+        
+        rigidBody.AddForce(dir ,ForceMode.Impulse);
+
+	}
+    private Vector3 Seperate()
+    {
+        Vector3 vec=new Vector3();
+        var position = this.transform.position;
+        int count = 0;
+        for (int i = 0; i < target.followers.Count; i++)
+        {
+            if (this.target.followers[i]!=this)
+            {
+                var tmp= target.followers[i].transform.position-position;
+                if (tmp.sqrMagnitude<minDistacne)
+                {
+                    vec += tmp;
+                    count++;
+                }
+
+            }
+        }
+        if (count > 0)
+            vec /= count;
+        return vec;
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector2 xz = new Vector2(rigidBody.velocity.x, rigidBody.velocity.z);
+        if (xz.sqrMagnitude > maxSpeed)
+        {
+            xz = xz.normalized * maxSpeed;
+            Vector3 velocity = new Vector3(xz.x,rigidBody.velocity.y,xz.y);
+            rigidBody.velocity = velocity;
+        }
+
+    }
+}
---
+++ GGJ18/Assets/FlockManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour {
+    public List<Boid> followers;
+    public Material queenMaterial;
+    public GameObject queen;
+	// Use this for initialization
+	void Awake() {
+        followers = new List<Boid>();
+	}
+
+    private void Start()
+    {
+        ChooseTheQueen();
+    }
+    void ChooseTheQueen()
+    {
+        var newQueen = followers[Random.Range(0, followers.Count)];
+        followers.Remove(newQueen);
+        queen=newQueen.gameObject;
+        Destroy(newQueen);
+        queen.transform.parent = this.transform;
+        queen.AddComponent<FlockQueen>();
+        queen.GetComponent<MeshRenderer>().material = queenMaterial;
+        queen.name = "queen";
+    }
+	void Update () {
+		
+	}
+
+
+
+}
