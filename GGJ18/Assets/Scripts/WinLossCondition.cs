0cf1b8727479ef8a95b46e31eb79140d5a1364af
hossamshuk <hossam.shuk@gmail.com>
2018-01-28 14:45:10 +0200

    added gamemanager script to game manager to handle win conditions

---
+++ GGJ18/Assets/Scripts/WinLossCondition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLossCondition : MonoBehaviour {
+
+    ZombieFlockManager zombieManager;
+    HumanManager humanManager;
+
+    private void Start()
+    {
+        zombieManager = this.gameObject.GetComponent<ZombieFlockManager>();
+        humanManager = this.gameObject.GetComponent<HumanManager>();
+    }
+
+    private void Update()
+    {
+        if(zombieManager.followers.Count == 0 && humanManager.followers.Count >0)
+        {
+            //humans win
+        }
+        else if (humanManager.followers.Count == 0)
+        {
+            //zombies win
+            //Doesn't put into consideration neutral humans
+        }
+    }
+}
